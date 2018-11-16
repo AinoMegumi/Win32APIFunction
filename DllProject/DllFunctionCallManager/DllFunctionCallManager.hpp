09a580d74d23bbbeb6da54654d2d7dfd00f0a0a4
AinoMegumi <aimegu2014@gmail.com>
2018-11-16 16:02:41 +0900

    DllFunctionCallManagerの追加とWin32Exceptionのプロジェクトの警告レベルの変更

---
+++ DllProject/DllFunctionCallManager/DllFunctionCallManager.hpp
@@ -0,0 +1,35 @@
+﻿#ifndef __DLLFUNCTIONCALLMANAGER_HPP__
+#define __DLLFUNCTIONCALLMANAGER_HPP__
+#ifdef DLLFUNCTIONCALLMANAGER_EXPORTS
+#define DLLFUNCMGR __declspec(dllexport)
+#else
+#define DLLFUNCMGR __declspec(dllimport)
+#endif
+#include <Windows.h>
+#include <string>
+
+class DllFunctionCallManager {
+private:
+	HMODULE hModule;
+	DLLFUNCMGR FARPROC GetFunctionAddressImpl(const std::string& FunctionName) const;
+	DLLFUNCMGR FARPROC GetFunctionAddressImpl(const std::wstring& FunctionName) const;
+public:
+	DLLFUNCMGR DllFunctionCallManager() : hModule(nullptr) {}
+	DLLFUNCMGR DllFunctionCallManager(const std::string ModuleName);
+	DLLFUNCMGR DllFunctionCallManager(const std::wstring ModuleName);
+	DLLFUNCMGR ~DllFunctionCallManager();
+	DllFunctionCallManager(const DllFunctionCallManager&) = delete;
+	DLLFUNCMGR DllFunctionCallManager(DllFunctionCallManager&& dcm);
+	DllFunctionCallManager& operator = (const DllFunctionCallManager&) = delete;
+	DLLFUNCMGR DllFunctionCallManager& operator = (DllFunctionCallManager&& dcm);
+	template<typename Func>
+	Func GetFunctionAddress(const std::string& FunctionName) const {
+		return reinterpret_cast<Func>(this->GetFunctionAddress());
+	}
+	template<typename Func>
+	Func GetFunctionAddress(const std::wstring& FunctionName) const {
+		return reinterpret_cast<Func>(this->GetFunctionAddress());
+	}
+};
+#endif
+
---
+++ StaticProject/DllFunctionCallManager/DllFunctionCallManager.hpp
@@ -0,0 +1,30 @@
+﻿#ifndef __DLLFUNCTIONCALLMANAGER_HPP__
+#define __DLLFUNCTIONCALLMANAGER_HPP__
+#include <Windows.h>
+#include <string>
+
+class DllFunctionCallManager {
+private:
+	HMODULE hModule;
+	FARPROC GetFunctionAddressImpl(const std::string& FunctionName) const;
+	FARPROC GetFunctionAddressImpl(const std::wstring& FunctionName) const;
+public:
+	DllFunctionCallManager() : hModule(nullptr) {}
+	DllFunctionCallManager(const std::string ModuleName);
+	DllFunctionCallManager(const std::wstring ModuleName);
+	~DllFunctionCallManager();
+	DllFunctionCallManager(const DllFunctionCallManager&) = delete;
+	DllFunctionCallManager(DllFunctionCallManager&& dcm);
+	DllFunctionCallManager& operator = (const DllFunctionCallManager&) = delete;
+	DllFunctionCallManager& operator = (DllFunctionCallManager&& dcm);
+	template<typename Func>
+	Func GetFunctionAddress(const std::string& FunctionName) const {
+		return reinterpret_cast<Func>(this->GetFunctionAddress());
+	}
+	template<typename Func>
+	Func GetFunctionAddress(const std::wstring& FunctionName) const {
+		return reinterpret_cast<Func>(this->GetFunctionAddress());
+	}
+};
+#endif
+
